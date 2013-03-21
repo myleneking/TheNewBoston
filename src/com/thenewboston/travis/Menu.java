ce9a82fa1c3463a6b8fc07d66a4f2791ff0c9b6d
Mylene King <mylene.king@gmail.com>
2013-03-21 14:42:35 +0800

    camera application (38-43)

--- src/com/thenewboston/travis/Menu.java
+++ src/com/thenewboston/travis/Menu.java
@@ -9,7 +9,7 @@ import android.widget.ListView;
 
 public class Menu extends ListActivity {
 
-	String classes[] = { "StartingPoint", "TextPlay", "Menu", "example3", "example4", "example5", "example6" };
+	String classes[] = { "StartingPoint", "TextPlay", "Menu", "Email", "Camera", "example5", "example6" };
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
